fecadbe855668943fb3abeffe7a1c38156f2a49d
rstock08 <ReedStock1992@gmail.com>
2019-07-08 09:42:01 -0700

    Section 2: Welcome to React | First React Application Videos: 4 - 7
    - Created a template react.js project
       - Walked through how ReactDOM library is used to inject elements from a *.js into a given *.html file by element id
    - Began creating a new index.html page

---
+++ portfolio/src/index.js
@@ -0,0 +1,8 @@
+// Imports react library
+import React from 'react';
+
+// Imports react-dom library which allows the insertion of index.js into index.html
+import ReactDOM from 'react-dom';
+
+// Imports the div into the root element located in index.html
+ReactDOM.render(<div>React Element</div>, document.getElementById('root'));
